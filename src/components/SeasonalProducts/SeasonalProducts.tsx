0c92a221d2ba67ba567df4ddfe689e3f674c55da
Vania Yarovyi <yarovyi@gmail.com>
2023-09-05 17:23:32 +0300

    added logic while pressing the btn

--- src/components/SeasonalProducts/SeasonalProducts.tsx
+++ src/components/SeasonalProducts/SeasonalProducts.tsx
@@ -6,12 +6,15 @@ import {
     TouchableOpacity,
     View
 } from 'react-native';
-import { Product } from '../../types/types';
-import { getSeasonalProducts } from '../../helpers/helper';
+import { getId, getSeasonalProducts } from '../../helpers/helper';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { add } from '../../reducers/products';
 
 const SeasonalProducts = () => {
     const [seasonProds, setSeasonProds] = useState([]);
     const [isSeasonalLoading, setIsSeasonalLoading] = useState(false);
+    const dispatch = useAppDispatch();
+    const prods = useAppSelector((state) => state.products)
 
     useEffect(() => {
         setIsSeasonalLoading(true)
@@ -32,13 +35,22 @@ const SeasonalProducts = () => {
                     color="green"
                 />
                 : <View>
-                    <Text>The following products are in high demand this season:</Text>
+                    <Text>
+                        {seasonProds.length > 0 && (
+                            'The following products are in high demand this season:'
+                        )}
+                    </Text>
                     <View
                         style={styles.seasonalBox}
                     >
-                        {seasonProds.map(prod => 
+                        {seasonProds.map((prod, i) => 
                             <TouchableOpacity
+                            key={i}
                             style={styles.seasonalBtn}
+                            onPress={() => {
+                                setSeasonProds((prev) => prev.filter(p => p !== prod))
+                                dispatch(add({id: getId(prods), title: prod, inCart: false}))
+                            }}
                             >
                                 <Text style={styles.seasonalText}>+ {prod}</Text>
                             </TouchableOpacity>
