0e841ba9db1c7e2cf18e489986b8017c586d3112
niaYaro <yarovyi@gmail.com>
2023-09-07 09:30:36 +0300

    changed status bar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import { View } from 'react-native';
 import { globalStyles } from '../../styles/global';
 import MainPage from '../MainPage/MainPage';
+import { SeasonalOffersProvider } from '../contexts/SeasonalOffersContext';
+import ListHeader from '../ListHeader/ListHeader';
+import SeasonalOffersBlock from '../SeasonalOffersBlock/SeasonalOffersBlock';
+import NewproductInput from '../NewproductInput/NewproductInput';
+import FinalModal from '../FinalModal/FinalModal';
+import { useAppSelector } from '../../hooks';
+import List from '../List/List';
+import { StatusBar } from 'expo-status-bar'
 
 const Layout = () => {
+    const prods = useAppSelector((state) => state.products);
+
     return (
-        <View style={globalStyles.container}>
-            <MainPage/>
-		</View>
+        // <View style={globalStyles.container}>
+        //     <MainPage/>
+		// </View>
+        <SeasonalOffersProvider>
+            <View style={globalStyles.container}>
+                <StatusBar
+                    // animated={true}
+                    backgroundColor="green"
+                />
+                <ListHeader />
+                <SeasonalOffersBlock />
+                {prods && 
+                    <List />
+                }
+                <NewproductInput />
+                <FinalModal />
+            </View>
+        </SeasonalOffersProvider>
     )
 }
 
